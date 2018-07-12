2d5237862cd09beb818ea6ac27d0128f6c859555
hevelmo <hevelmo060683@gmail.com>
2018-07-12 18:22:27 -0500

    Initial commit

---
+++ data/index.php
@@ -0,0 +1,410 @@
+<?php
+	require "json-file-decode.class.php";
+	$catalogo = array(
+		"catalogo" => array(
+			array(
+				"data" => "1",
+				"class" => "hard",
+				"nombre" => "portada",
+				"numero" => "page-01",
+				"image" => "CATALOGO GHIA 2018 ABRIL.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-01"
+			),
+			array(
+				"data" => "2",
+				"class" => "inner",
+				"nombre" => "second",
+				"numero" => "page-02",
+				"image" => "CATALOGO GHIA 2018 ABRIL2.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-02"
+			),
+			array(
+				"data" => "3",
+				"class" => "page-03",
+				"nombre" => "productos",
+				"numero" => "page-03",
+				"image" => "CATALOGO GHIA 2018 ABRIL3.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-03",
+				"coords" => array(
+					array(
+						"coord" => "23, 325, 374, 578"
+					),
+					array(
+						"coord" => "23, 58, 374, 311"
+					)
+				)
+			),
+			array(
+				"data" => "4",
+				"class" => "page-04",
+				"nombre" => "productos",
+				"numero" => "page-04",
+				"image" => "CATALOGO GHIA 2018 ABRIL4.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-04",
+				"coords" => array(
+					array(
+						"coord" => "23, 77, 374, 330"
+					),
+					array(
+						"coord" => "23, 391, 374, 578"
+					)
+				)
+			),
+			array(
+				"data" => "5",
+				"class" => "page-05",
+				"nombre" => "productos",
+				"numero" => "page-05",
+				"image" => "CATALOGO GHIA 2018 ABRIL5.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-05",
+				"coords" => array(
+					array(
+						"coord" => "15, 49, 381, 581"
+					)
+				)
+			),
+			array(
+				"data" => "6",
+				"class" => "page-06",
+				"nombre" => "productos",
+				"numero" => "page-06",
+				"image" => "CATALOGO GHIA 2018 ABRIL6.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-06",
+				"coords" => array(
+					array(
+						"coord" => "16, 60, 387, 579"
+					)
+				)
+			),
+			array(
+				"data" => "7",
+				"class" => "page-07",
+				"nombre" => "productos",
+				"numero" => "page-07",
+				"image" => "CATALOGO GHIA 2018 ABRIL7.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-07",
+				"coords" => array(
+					array(
+						"coord" => "20, 66, 383, 581"
+					)
+				)
+			),
+			array(
+				"data" => "8",
+				"class" => "page-08",
+				"nombre" => "productos",
+				"numero" => "page-08",
+				"image" => "CATALOGO GHIA 2018 ABRIL8.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-08",
+				"coords" => array(
+					array(
+						"coord" => "16, 59, 380, 585"
+					)
+				)
+			),
+			array(
+				"data" => "9",
+				"class" => "page-09",
+				"nombre" => "productos",
+				"numero" => "page-09",
+				"image" => "CATALOGO GHIA 2018 ABRIL9.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-09",
+				"coords" => array(
+					array(
+						"coord" => "15, 55, 387, 469"
+					)
+				)
+			),
+			array(
+				"data" => "10",
+				"class" => "page-10",
+				"nombre" => "productos",
+				"numero" => "page-10",
+				"image" => "CATALOGO GHIA 2018 ABRIL10.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-10",
+				"coords" => array(
+					array(
+						"coord" => "14, 58, 383, 476"
+					)
+				)
+			),
+			array(
+				"data" => "11",
+				"class" => "page-11",
+				"nombre" => "productos",
+				"numero" => "page-11",
+				"image" => "CATALOGO GHIA 2018 ABRIL11.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-11",
+				"coords" => array(
+					array(
+						"coord" => "15, 57, 379, 478"
+					)
+				)
+			),
+			array(
+				"data" => "12",
+				"class" => "page-12",
+				"nombre" => "productos",
+				"numero" => "page-12",
+				"image" => "CATALOGO GHIA 2018 ABRIL12.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-12",
+				"coords" => array(
+					array(
+						"coord" => "213, 375, 384, 597"
+					),
+					array(
+						"coord" => "9, 385, 212, 597"
+					),
+					array(
+						"coord" => "207, 195, 392, 375"
+					),
+					array(
+						"coord" => "16, 194, 206, 386"
+					),
+					array(
+						"coord" => "129, 23, 376, 194s"
+					)
+				)
+			),
+			array(
+				"data" => "13",
+				"class" => "page-13",
+				"nombre" => "productos",
+				"numero" => "page-13",
+				"image" => "CATALOGO GHIA 2018 ABRIL13.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-13",
+				"coords" => array(
+					array(
+						"coord" => "21, 54, 387, 581"
+					)
+				)
+			),
+			array(
+				"data" => "14",
+				"class" => "page-14",
+				"nombre" => "productos",
+				"numero" => "page-14",
+				"image" => "CATALOGO GHIA 2018 ABRIL14.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-14",
+				"coords" => array(
+					array(
+						"coord" => "17, 55, 375, 586"
+					)
+				)
+			),
+			array(
+				"data" => "15",
+				"class" => "page-15",
+				"nombre" => "productos",
+				"numero" => "page-15",
+				"image" => "CATALOGO GHIA 2018 ABRIL15.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-15",
+				"coords" => array(
+					array(
+						"coord" => "26, 402, 371, 592"
+					),
+					array(
+						"coord" => "19, 210, 375, 403"
+					),
+					array(
+						"coord" => "22, 71, 376, 208"
+					)
+				)
+			),
+			array(
+				"data" => "16",
+				"class" => "page-16",
+				"nombre" => "productos",
+				"numero" => "page-16",
+				"image" => "CATALOGO GHIA 2018 ABRIL16.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-16",
+				"coords" => array(
+					array(
+						"coord" => "23, 321, 370, 589"
+					),
+					array(
+						"coord" => "23, 67, 368, 318"
+					)
+				)
+			),
+			array(
+				"data" => "17",
+				"class" => "page-17",
+				"nombre" => "productos",
+				"numero" => "page-17",
+				"image" => "CATALOGO GHIA 2018 ABRIL17.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-17",
+				"coords" => array(
+					array(
+						"coord" => "24, 422, 370, 587"
+					),
+					array(
+						"coord" => "19, 226, 367, 422"
+					),
+					array(
+						"coord" => "19, 60, 367, 221"
+					)
+				)
+			),
+			array(
+				"data" => "18",
+				"class" => "page-18",
+				"nombre" => "productos",
+				"numero" => "page-18",
+				"image" => "CATALOGO GHIA 2018 ABRIL18.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-18",
+				"coords" => array(
+					array(
+						"coord" => "20, 434, 376, 592"
+					),
+					array(
+						"coord" => "21, 225, 376, 430"
+					),
+					array(
+						"coord" => "23, 68, 375, 221"
+					)
+				)
+			),
+			array(
+				"data" => "19",
+				"class" => "page-19",
+				"nombre" => "productos",
+				"numero" => "page-19",
+				"image" => "CATALOGO GHIA 2018 ABRIL19.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-19",
+				"coords" => array(
+					array(
+						"coord" => "14, 319, 377, 583"
+					),
+					array(
+						"coord" => "13, 61, 377, 315"
+					)
+				)
+			),
+			array(
+				"data" => "20",
+				"class" => "page-20",
+				"nombre" => "productos",
+				"numero" => "page-20",
+				"image" => "CATALOGO GHIA 2018 ABRIL20.jpg",
+				"url" => "img/libro",
+				"area" => "map-page-20",
+				"coords" => array(
+					array(
+						"coord" => "261, 200, 392, 493"
+					),
+					array(
+						"coord" => "130, 220, 251, 492"
+					),
+					array(
+						"coord" => "19, 278, 122, 485"
+					)
+				)
+			)
+		)
+	);
+	$url = $catalogo;
+	//$data = file_get_contents($url);
+	$characters = json_encode($url);
+	print_r("<pre>". $characters ."</pre>");
+
+	$handler = fopen("catalogo.json", "w+");
+	fwrite($handler, $characters);
+	fclose($handler);
+
+	$read = new json_file_decode();
+	$json = $read->json("catalogo.json");
+
+	$dec_catalogo = $json["catalogo"];
+
+	//print_r($dec_catalogo);
+
+	//print_r($dec_catalogo[0]);
+
+	/*
+	foreach ($json as $key => $val) {
+	    if ($key == "catalogo") {
+	    	print_r("<pre>". $json[0] . "</pre>");
+		  	foreach ($json[$key][0] as $i => $v) {
+			  	print_r("<pre>". $json[$key][$i][$v] . "</pre>");
+			  	print_r("<pre>". $i . "=>" . $v . "</pre>");
+		  	}
+	 	}
+	}
+	*/
+	?>
+	<div class="box" style="display: block; overflow: hidden; overflow-y: scroll; width: 100%; height: 300px;">
+	<?php
+	for ( $x = 0; $x < count($json["catalogo"]); $x++) {
+		$raiz = $json["catalogo"];
+		$cat_page_id = $raiz[$x]["data"];
+		$cat_page_class = $raiz[$x]["class"];
+		$cat_page_nombre = $raiz[$x]["nombre"];
+		$cat_page_numero = $raiz[$x]["numero"];
+		$cat_page_image = $raiz[$x]["image"];
+		$cat_page_url = $raiz[$x]["url"];
+		$cat_page_area = $raiz[$x]["area"];
+
+		echo $cat_page_id;
+		echo "<br>";
+		echo $cat_page_class;
+		echo "<br>";
+		echo $cat_page_nombre;
+		echo "<br>";
+		echo $cat_page_numero;
+		echo "<br>";
+		echo $cat_page_image;
+		echo "<br>";
+		echo $cat_page_url;
+		echo "<br>";
+		echo $cat_page_area;
+		echo "<br>";
+		for ($x1 = 0; $x1 < count($raiz[$x]['coords']);  $x1++) {
+			$cat_page_area_id = $raiz[$x]['coords'][$x1]['coord'];
+			echo $cat_page_area_id;
+			echo "<br>";
+		}
+		echo "<br>";
+	}
+	?>
+	</div>
+	<?php
+	
+	/*
+	$catalogo = $dec_catalogo;
+	foreach ($catalogo as $x => $value) {
+		echo "Valor 1: ";
+		echo $x .'=>'. $value;
+		echo "<br>";
+		foreach ($catalogo[$x]['area'] as $x2 => $value2) {
+			echo "valor 2";
+			echo $x2 .'=>'. $value2;
+			echo "<br>";
+			foreach ($catalogo[$x]['area'][$x2]['coords'] as $x3 => $value3) {
+				echo "valor 3";
+				echo $x3 .'=>'. $value3;
+				echo "<br>";
+			}
+		}
+	}
+	*/
+?>
